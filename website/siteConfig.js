ef93e42f380c7c6fda72b52c5ee0bb9f89211a99
Khosi <mahdi0khosravi@gmail.com>
2019-02-22 02:27:45 +0330

    now we have automation

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'Path.WTF', // Title for your website.
   tagline: 'It`s like a map',
-  url: 'https://path.wtf', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://mahdikhosravi.github.io', // Your website URL
+  baseUrl: '/path.wtf/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
   projectName: 'path.wtf',
-  organizationName: 'path.wtf',
+  organizationName: 'mahdikhosravi',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -54,8 +54,8 @@ const siteConfig = {
 
   /* Colors for website */
   colors: {
-    primaryColor: '#2E8555',
-    secondaryColor: '#205C3B',
+    primaryColor: '#ffc300',
+    secondaryColor: '#1f1f1f',
   },
 
   /* Custom fonts for website */
