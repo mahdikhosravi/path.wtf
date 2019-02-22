0340a295ca59201f486fc027cca078a13167cdb2
Khosi <mahdi0khosravi@gmail.com>
2019-02-22 17:30:54 +0330

    siteconfig changed

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'path.wtf',
+  projectName: 'path-wtf.github.io',
   organizationName: 'path-wtf',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
